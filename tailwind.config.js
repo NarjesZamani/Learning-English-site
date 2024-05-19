0d45832fb3858da94c2eef95485d9e9191deb367
NarjesZamani <narjeszamani2001@gmail.com>
2024-05-19 18:04:27 +0430

    addTailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "DanaReg": "Dana Regular",
+        "DanaMed": "Dana Medium",
+        "DanaBold":" Dana Bold",
+      },
+    },
+  },
+  plugins: [],
+}
+
